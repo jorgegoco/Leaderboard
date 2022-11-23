8043be3f566dc57f25a59c52d6942e207082259b
jorgegoco <jorgegoco70@gmail.com>
2022-11-23 11:21:47 +0100

    Added 'async' and 'await' promises

--- src/index.js
+++ src/index.js
@@ -4,41 +4,36 @@ const API_KEY = 'QMhictd9pPqlR9FLpNfG';
 
 const form = document.getElementById('form');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const data = {};
   data.user = form.elements[0].value;
   data.score = form.elements[1].value;
   form.reset();
 
-
-  fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${API_KEY}/scores/`, {
+  await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${API_KEY}/scores/`, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
 });
 
 const button = document.querySelector('button');
 const ul = document.querySelector('ul');
 
-button.addEventListener('click', () => {
-  fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${API_KEY}/scores/`)
-    .then((response) => response.json())
-    .then((json) => {
-      const allData = json.result;
-      ul.replaceChildren();
-      for(let i = 0; i < allData.length; i += 1){
-        let user = allData[i].user;
-        let score = allData[i].score;
-        const li = document.createElement('li');
-        let text = document.createTextNode(`${user}: ${score}`);
-        li.appendChild(text);
-        ul.appendChild(li);
-      }
-    })
+button.addEventListener('click', async () => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${API_KEY}/scores/`);
+  const jsonResponse = await response.json();
+  const allData = jsonResponse.result;
+  ul.replaceChildren();
+  for (let i = 0; i < allData.length; i += 1) {
+    let user = allData[i].user;
+    let score = allData[i].score;
+    const li = document.createElement('li');
+    let text = document.createTextNode(`${user}: ${score}`);
+    li.appendChild(text);
+    ul.appendChild(li);
+  }
 });
